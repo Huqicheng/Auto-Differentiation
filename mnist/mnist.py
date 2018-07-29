fix gradient of the bias of fc layer

mnist/mnist.py
@@ -1,60 +1,45 @@
-import gzip
-import struct
-import random
 import numpy as np
+from urllib import request
+import gzip
+import pickle
+import os
 
+filename = [
+            ["training_images","train-images-idx3-ubyte.gz"],
+            ["test_images","t10k-images-idx3-ubyte.gz"],
+            ["training_labels","train-labels-idx1-ubyte.gz"],
+            ["test_labels","t10k-labels-idx1-ubyte.gz"]
+            ]
 
+def download_mnist():
+    base_url = "http://yann.lecun.com/exdb/mnist/"
+    for name in filename:
+        print("Downloading "+name[1]+"...")
+        request.urlretrieve(base_url+name[1],"./mnist/"+name[1])
+    print("Download complete.")
 
-import sys
-sys.path.append("../")
-from qnet.layer import Linear
-from qnet.nn import Sequential
-from qnet.train import compile, train
-from qnet.autodiff import Variable
-from qnet.loss import SoftmaxCrossEntropy
-model = Sequential(
-        name="net",
-        layers = [
-            Linear(name="linear_1", input_size=2, output_size=2)
-        ]
-    )
+def save_mnist():
+    mnist = {}
+    for name in filename[:2]:
+        with gzip.open("./mnist/"+name[1], 'rb') as f:
+            mnist[name[0]] = np.frombuffer(f.read(), np.uint8, offset=16).reshape(-1,28*28)
+    for name in filename[-2:]:
+        with gzip.open("./mnist/"+name[1], 'rb') as f:
+            mnist[name[0]] = np.frombuffer(f.read(), np.uint8, offset=8)
+    with open("./mnist/mnist.pkl", 'wb') as f:
+        pickle.dump(mnist,f)
+    print("Save complete.")
 
+def init():
+    download_mnist()
+    save_mnist()
 
+def load():
+    if os.path.exists("./mnist/mnist.pkl") == False:
+        init()
+    with open("./mnist/mnist.pkl",'rb') as f:
+        mnist = pickle.load(f)
+    return mnist["training_images"], mnist["training_labels"], mnist["test_images"], mnist["test_labels"]
 
-
-inputs = Variable(name="x")
-targets = Variable(name="y")
-
-
-inputs, targets, loss, grad_node_list = compile(net=model, inputs=inputs, targets=targets, loss=SoftmaxCrossEntropy())
-
-print(loss)
-
-timage = np.array([
-                   [0, 0],
-                   [1, 0],
-                   [0, 1],
-                   [1, 1],
-                   
-                   ], dtype='float32')
-
-tlabel = np.array([
-                    [1, 0],
-                    [0, 1],
-                    [0, 1],
-                    [1, 0],
-                    
-                    ], dtype='float32')
-train(model, inputs, targets, loss, grad_node_list,
-          timage,
-          tlabel,
-          num_epochs=800
-          )
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    init()